b25344537bb47c4bd1b4e9ab358d2ec4426b1bbe
Joefa Cao <cao11d@uwindsor.ca>
2019-06-03 15:44:53 -0400

    recommit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
 app.use(logger('dev'));
@@ -11,5 +12,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.get('*', (req, res) =>{
     res.status(200).send(`Welcome to the beginning`);
 })
+
+
+
 
 module.exports = app;
